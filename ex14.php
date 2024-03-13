26d270fdbede98fc5129b89f71c8961ab9ec2249
Malak Hannachi <malakhannachi@gmail.com>
2024-03-13 16:47:03 +0100

    exercice 14 Héritage

--- ex14.php
+++ ex14.php
@@ -1,18 +1,35 @@
 <?php
 $v1 = new Voiture("Peugeot","408");
+$v2 = new VoitureElec ("BMW","I3",100);
 
 echo $v1->getInfos()."<br/>";
+echo $v2->getInfos()."<br/>";
+
 class Voiture{
 
-    private string $marque; 
-    private string $modéle;
+    protected string $marque; 
+    protected string $modéle;
 
     public function __construct(string $marque, string $modéle){
         $this ->marque = $marque;
         $this ->modéle = $modéle;
        
     }
-    public function getInfo(){
+    public function getInfos(){
         return $this ->marque. $this ->modéle;
 }
 }
+class VoitureElec extends Voiture{
+
+    private int $batterie;
+
+    public function __construct(string $marque, string $modéle,  int $batterie ){
+       
+        parent::__construct( $marque,  $modéle);
+        $this ->batterie = $batterie ;
+    }
+    public function getInfos(){
+        return $this ->marque. $this ->modéle.$this->batterie;
+    }
+}
+
